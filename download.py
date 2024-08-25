4d9e1be852afe28d7faf709e3049e6ad61ccd0bb
Reza <reza.forouzan@gmail.com>
2024-08-25 20:53:20 +0330

    first step

--- download.py
+++ download.py
@@ -1,20 +1,40 @@
+# import xlrd
 import pandas as pd
 import boto3
+# f = xlrd.open_workbook("/mnt/data/Work/Kian/kyc/dataset2-image-id.xlsx")
+# f = pd.ExcelFile("/mnt/data/Work/Kian/kyc/dataset2-image-id.xlsx")
+# print (type(f))
+# fline = f.readlines()
+# for line in fline:
+#     print (line)
 
-f = pd.read_excel("/mnt/data/Work/Kian/kyc/s3/test.xlsx")
+# dfs = pd.read_excel(file_name, sheet_name=None)
 
+f = pd.read_excel("/mnt/data/Work/Kian/kyc/test.xlsx")
+# print (f)
+# print (type(f))
+# print (f.iloc[0])
+# print (f.iat[0,0])
+# print (f.iat[1,0])
 
-ak = input("please enter s3 Access Key: ")
-sk = input("please enter s3 Secret Key: ")
-bk = input("Please enter s3 Bucket name: ")
-endpoint = input("Please enter s3 Endpoint URL: ")
-s3 = boto3.client('s3',endpoint_url=endpoint,aws_access_key_id=ak,aws_secret_access_key=sk)
+# for i in range (0,len(f)):
+#     print (f.iat[i,0])
 
-try:
-    for i in range (0,len(f)):
-        st = str(f.values[i])
-        st = st.replace("']","")
-        st = st.replace("['","")
-        s3.download_file(bk,st,"/mnt/data/Work/Kian/kyc/images/s3/%s" % (st))
-except Exception as e:
-    print (e)
+# c = 0
+# for rows in f:
+#     print (f.values)
+#     c += 1
+# print (c)
+
+for i in range (0,len(f)):
+    # print (f.values[i])
+    # print (type(f.values[i]))
+    # print (str(f.values[i]))
+    # print (type(str(f.values[i])))
+    # c += 1
+    st = str(f.values[i])
+    st = st.replace("']","")
+    st = st.replace("['","")
+
+    # print (st)
+# print (c)
